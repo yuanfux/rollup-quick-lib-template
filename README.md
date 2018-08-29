b84773159f3a1e61d4548fcbea4ef0f8335737ba
Yuan Fu <fuyuanx@gmail.com>
2018-08-29 16:33:44 +0800

    feat: sourcemap for dev

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rollup-quick-lib-template
+
+> If you are using es6 to build a quick js lib tool, then this is the go-to rollup template
+
+```
+yarn add
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,36 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
+import { basename } from 'path';
 
-const libName = 'bundle';
+const isDev = process.env.NODE_ENV === 'development';
 
 export default {
   input: 'src/index.js',
   output: [
-  	{ file: 'dist/' + libName + '.js', format: 'cjs'},
-  	{ file: 'dist/' + libName + '.m.js', format: 'esm'},
-  	{ file: 'dist/' + libName + '.umd.js', format: 'umd', name: libName}
+  	{ 
+  		file: pkg['main'],
+  		format: 'cjs'
+  	},
+  	{ 
+  		file: pkg['module'],
+  		format: 'esm'
+  	},
+  	{ 
+  		file: pkg['umd:main'],
+  		format: 'umd',
+  		name: basename(pkg['umd:main']).replace(/\.umd\.js$/, '')
+  	}
   ],
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }),
-    uglify()
+    uglify({
+    	sourceMap: isDev ? true : false
+    })
   ],
   watch: {
   	exclude: 'node_modules/**'
